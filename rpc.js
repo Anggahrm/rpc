aa8ab695b8b2c4af53ed293fbf0cd6ef7c98ff02
Anggahrm <58013844+Anggahrm@users.noreply.github.com>
2025-08-29 10:20:19 +0700

    Update RichPresence details for workspace context

--- rpc.js
+++ rpc.js
@@ -9,11 +9,11 @@ client.on('ready', async () => {
     .setApplicationId('1380551344515055667')
     .setType('PLAYING')
     .setURL('https://www.youtube.com/watch?v=5icFcPkVzMg') // If you set a URL, it will automatically change to STREAMING type
-    .setState('Arcade Game')
-    .setName('osu!')
-    .setDetails('MariannE - Yooh')
+    .setState('Workspace: ZumyNext')
+    .setName('Visual Studio Code')
+    .setDetails('Editing main.js')
     .setStartTimestamp(Date.now())
-    .setAssetsLargeImage('Visual Studio Code') // https://assets.ppy.sh/beatmaps/1550633/covers/list.jpg
+    .setAssetsLargeImage('1380551344515055667') // https://assets.ppy.sh/beatmaps/1550633/covers/list.jpg
     .setAssetsLargeText('Idle')
     .setAssetsSmallImage('1380551344515055667') // https://discord.com/api/v9/oauth2/applications/367827983903490050/assets
     .setAssetsSmallText('click the circles')
