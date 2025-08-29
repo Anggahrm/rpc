f4d503fcd486c2263f3cac049af7fa0d04d14f5d
Anggahrm <58013844+Anggahrm@users.noreply.github.com>
2025-08-29 09:46:35 +0700

    Update Discord Rich Presence application ID and assets

--- rpc.js
+++ rpc.js
@@ -5,11 +5,11 @@ client.on('ready', async () => {
   console.log(`${client.user.username} is ready!`);
   const getExtendURL = await RichPresence.getExternal(
     client,
-    '367827983903490050',
-    'https://assets.ppy.sh/beatmaps/1550633/covers/list.jpg', // Required if the image you use is not in Discord
+    '1380551344515055667',
+    //'https://assets.ppy.sh/beatmaps/1550633/covers/list.jpg', // Required if the image you use is not in Discord
   );
   const status = new RichPresence(client)
-    .setApplicationId('367827983903490050')
+    .setApplicationId('1380551344515055667')
     .setType('PLAYING')
     .setURL('https://www.youtube.com/watch?v=5icFcPkVzMg') // If you set a URL, it will automatically change to STREAMING type
     .setState('Arcade Game')
@@ -22,27 +22,13 @@ client.on('ready', async () => {
     .setStartTimestamp(Date.now())
     .setAssetsLargeImage(getExtendURL[0].external_asset_path) // https://assets.ppy.sh/beatmaps/1550633/covers/list.jpg
     .setAssetsLargeText('Idle')
-    .setAssetsSmallImage('373370493127884800') // https://discord.com/api/v9/oauth2/applications/367827983903490050/assets
+    .setAssetsSmallImage('1380551344515055667') // https://discord.com/api/v9/oauth2/applications/367827983903490050/assets
     .setAssetsSmallText('click the circles')
     .setPlatform('desktop')
-    .addButton('Beatmap', 'https://osu.ppy.sh/beatmapsets/1391659#osu/2873429');
-  // Custom Status
-  const custom = new CustomStatus(client).setEmoji('??').setState('yum');
-  // Spotify
-  const spotify = new SpotifyRPC(client)
-    .setAssetsLargeImage('spotify:ab67616d00001e02768629f8bc5b39b68797d1bb') // Image ID
-    .setAssetsSmallImage('spotify:ab6761610000f178049d8aeae802c96c8208f3b7') // Image ID
-    .setAssetsLargeText('未来茶屋 (vol.1)') // Album Name
-    .setState('Yunomi; Kizuna AI') // Artists
-    .setDetails('ロボットハート') // Song name
-    .setStartTimestamp(Date.now())
-    .setEndTimestamp(Date.now() + 1_000 * (2 * 60 + 56)) // Song length = 2m56s
-    .setSongId('667eE4CFfNtJloC6Lvmgrx') // Song ID
-    .setAlbumId('6AAmvxoPoDbJAwbatKwMb9') // Album ID
-    .setArtistIds('2j00CVYTPx6q9ANbmB2keb', '2nKGmC5Mc13ct02xAY8ccS'); // Artist IDs
-
-  client.user.setPresence({ activities: [status, custom, spotify] });
+    .addButton('Community', 'https://discord.gg/W9qD2mYXxf');
+  
+  client.user.setPresence({ activities: [status] });
 });
 
-client.login('NjY0MTE0NjcwNTc5MzUxNTYy.G960SS.QfKmhla9c-fO0LJpsP_EUo_i55EKHQupzRCnBo');
+client.login('token');
     
